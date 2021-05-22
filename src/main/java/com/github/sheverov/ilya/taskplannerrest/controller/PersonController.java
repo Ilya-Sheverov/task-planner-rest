7540b4c92b85d4f568062462b28da49add7ef1d2
Ilya-Sheverov <ilya.sheverov@gmail.com>
2021-05-22 23:30:10 +0300

    Добавлены Dto.

--- src/main/java/com/github/sheverov/ilya/taskplannerrest/controller/PersonController.java
+++ src/main/java/com/github/sheverov/ilya/taskplannerrest/controller/PersonController.java
@@ -2,14 +2,18 @@ package com.github.sheverov.ilya.taskplannerrest.controller;
 
 import com.github.sheverov.ilya.taskplannerrest.PersonDTOService;
 import com.github.sheverov.ilya.taskplannerrest.dto.person.PersonDto;
+import com.github.sheverov.ilya.taskplannerrest.dto.person.PersonWithTasksDto;
 import com.github.sheverov.ilya.taskplannerrest.entity.Person;
 import java.util.List;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,17 +23,38 @@ public class PersonController {
     PersonDTOService personDTOService;
 
     @GetMapping(path = "/persons")
-    public List<PersonDto> getPersons() {
-        return personDTOService.getPersonsDTO();
+    public Map<String, Object> getPersons(
+        @RequestParam(defaultValue = "false") boolean includingTasks) {
+        if (includingTasks) {
+            List<PersonWithTasksDto> personWithTasksDTOs = personDTOService
+                .getPersonWithTasksDTOs();
+            return Map.of("persons", personWithTasksDTOs);
+        } else {
+            List<PersonDto> personDTOs = personDTOService.getPersonDTOs();
+            return Map.of("persons", personDTOs);
+        }
     }
 
     @GetMapping("/persons/{personId}")
-    public PersonDto getPerson(@PathVariable Integer personId) {
-        return personDTOService.getPersonDtoById(personId);
+    public Map<String, Object> getPerson(@PathVariable Integer personId,
+        @RequestParam(defaultValue = "false") boolean includingTasks) {
+        if (includingTasks) {
+            PersonWithTasksDto personWithTasksDto = personDTOService
+                .getPersonWithTasksDtoById(personId);
+            return Map.of("person", personWithTasksDto);
+        } else {
+            PersonDto personDto = personDTOService.getPersonDtoById(personId);
+            return Map.of("person", personDto);
+        }
     }
 
     @PostMapping("/persons")
     public void createPerson(@RequestBody @Validated Person person) {
         personDTOService.createPerson(person);
+    }
+
+    @DeleteMapping("/persons")
+    public void deletePerson(@RequestBody Person person) {
+        personDTOService.deletePerson(person);
     }
 }
